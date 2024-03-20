c8be13ee2a27e00e66d71b927347ebf4d158e90a
wisdomwest <harmonykoome@gmail.com>
2024-03-20 16:39:36 +0000

    lol uts 2022 not 2020

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 --  lists all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
