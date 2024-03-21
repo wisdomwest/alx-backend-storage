f6d95d54a358dfe76df6ba5e60b0cb3dc62e9529
wisdomwest <harmonykoome@gmail.com>
2024-03-21 15:26:11 +0000

    task 7 procedures

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,16 @@
+-- creates a stored procedure AddBonus that adds a new correction for a student.
+
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER $$
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
+BEGIN
+    DECLARE project_id INT DEFAULT 0;
+    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
+END
+$$
+DELIMITER ;
