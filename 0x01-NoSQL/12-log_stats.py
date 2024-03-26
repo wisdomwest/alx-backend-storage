eb84035bc3f2ef3fa1a7b32a1cd5a8c80151f02c
wisdomwest <harmonykoome@gmail.com>
2024-03-27 01:00:54 +0300

    okay

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -12,4 +12,24 @@ method=GET
 path=/status'''
 
 
-from pymongo import 
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+
+    count = logs.count_documents({})
+    print(f"{count} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+
+    for method in methods:
+        count = logs.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+
+    status = logs.count_documents({"method": "GET", "path": "/status"})
+
+    print(f"{status} status check")
+
