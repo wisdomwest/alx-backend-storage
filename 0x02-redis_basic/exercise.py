5f44af7cb98fd9054e871d930e2d4fcbace36ebc
wisdomwest <harmonykoome@gmail.com>
2024-03-27 15:58:27 +0300

    lets see

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,8 @@
 
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
+
 
 class Cache:
     '''Cache class'''
@@ -17,3 +18,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self,
+            key: str,
+            fn: callable = None,
+            ) -> Union[str, bytes, int, float]:
+        '''Get data from redis'''
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        '''Get string data from redis'''
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        '''Get int data from redis'''
+        return self.get(key, int)
