a20761e85eb7008c53e4752e21ea2952dd7d1eff
wisdomwest <harmonykoome@gmail.com>
2024-03-27 15:09:19 +0300

    task 0 classes quick and easy

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 '''Learning redis with python'''
+
+import uuid
+import redis
+from typing import Union
+
+class Cache:
+    '''Cache class'''
+    def __init__(self):
+        '''Constructor'''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''Store data in redis'''
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
